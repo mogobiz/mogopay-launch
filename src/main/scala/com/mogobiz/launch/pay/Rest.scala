190e40b003c2e92b9fafaeef7f77985b755cc974
Samy Dindane <samy@dindane.com>
2014-11-06 17:54:09 +0100

    Add MogopayRoutes' bootstrap

--- src/main/scala/com/mogobiz/launch/pay/Rest.scala
+++ src/main/scala/com/mogobiz/launch/pay/Rest.scala
@@ -8,14 +8,12 @@ import spray.can.Http
 object Rest extends App with BootedMogobizSystem with MogopayActors with MogopayRoutes {
   com.mogobiz.pay.jobs.ImportCountriesJob.start(system)
   com.mogobiz.pay.jobs.CleanAccountsJob.start(system)
-  com.mogobiz.pay.jobs.CleanTransactionRequestsJob.start(system)
 
   override val bootstrap = {
     super[MogopayRoutes].bootstrap()
     com.mogobiz.session.boot.DBInitializer()
     com.mogobiz.notify.boot.DBInitializer()
   }
-
 
   IO(Http)(system) ! Http.Bind(routesServices, interface = Settings.ServerListen, port = Settings.ServerPort)
 }
